3126a21ab67cb17930f8135d4b354f456d1b420d
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-10-02 16:33:11 -0300

    finalizando components/ menu mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
 import './Header.css';
 import Logo from '../Images/logo.svg';
+
 const Header=()=> {
+    const [active, setActive] = useState("nav-mobile");
+    const [toggleIcon, setToggleIcon] = useState("btn-mobile");
+    const navToggle = ()=> { 
+        active === 'nav-mobile' ? setActive('nav-mobile nav-active') : setActive('nav-mobile');
+        toggleIcon === 'btn-mobile' ? setToggleIcon('btn-mobile toggle') : setToggleIcon('btn-mobile');
+      };
+    const [close, setClose] = useState("close-menu");
+    const closeMenu=()=> {
+        close === "menu-close" ? setClose('menu-close nav__active') : setClose("menu-close");
+        active === 'nav-mobile' ? setActive('nav-mobile nav-active') : setActive('nav-mobile');
+        toggleIcon === 'btn-mobile' ? setToggleIcon('btn-mobile toggle') : setToggleIcon('btn-mobile');
+      }
     return (
         <div className="header">
             <div className="logo">
@@ -14,6 +29,21 @@ const Header=()=> {
                     <li><a href="#trending">Trending</a></li>
                     <li><a href="#categories">Categories</a></li>
                 </ul>
+            </nav>
+            <div onClick={navToggle} className={toggleIcon}>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
+            </div>
+            <nav className={active}>
+                <div onClick={closeMenu} className="close-menu">X</div>
+                <ul className={close}>
+                    <li><a href="./index.html">Home</a></li>
+                    <li><a href="#news">New</a></li>
+                    <li><a href="#popular">Popular</a></li>
+                    <li><a href="#trending">Trending</a></li>
+                    <li><a href="#categories">Categories</a></li>
+                </ul>   
             </nav>
         </div>  
         )
