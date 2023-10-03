808f82e0645cc8b8a7e4f07f93aad8d40f8ddcf0
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-10-03 10:40:18 -0300

    finalizando projeto

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,30 +7,36 @@ const Footer=()=> {
     return(
         <div>
             <div className='footer'>
-                <div className="footer-content">
-                    <div className="footer-image">
-                        <img src={Pcs} alt="computers"/>
+                <div className="footer-container">
+                    <div className="footer-content">
+                        <div className="footer-image">
+                            <img src={Pcs} alt="computers"/>
+                        </div>
+                        <div className="footer-info">
+                            <h1>01</h1>
+                            <h2>Reviving Retro PCs</h2>
+                            <p>What happens when old PCs are given modern upgrades?</p>
+                        </div>
                     </div>
-                    <div className="footer-info">
-                        <h1>01</h1>
-                        <h2>Reviving Retro PCs</h2>
-                        <p>What happens when old PCs are given modern upgrades?</p>
+                    <div className="footer-content">
+                        <div className="footer-image">
+                            <img src={Laptops} alt="laptops"/>
+                        </div>
+                        <div className="footer-info">
+                            <h1>02</h1>
+                            <h2>Top 10 Laptops of 2022</h2>
+                            <p>Our best picks for various needs and budgets.</p>
+                        </div>
                     </div>
-                    <div className="footer-image">
-                        <img src={Laptops} alt="laptops"/>
-                    </div>
-                    <div className="footer-info">
-                        <h1>02</h1>
-                        <h2>Top 10 Laptops of 2022</h2>
-                        <p>Our best picks for various needs and budgets.</p>
-                    </div>
-                    <div className="footer-image">
-                        <img src={Gaming} alt="gaming"/>
-                    </div>
-                    <div className="footer-info">
-                        <h1>03</h1>
-                        <h2>The Growth of Gaming</h2>
-                        <p>How the pandemic has sparked fresh opportunities.</p>
+                    <div className="footer-content">
+                        <div className="footer-image">
+                            <img src={Gaming} alt="gaming"/>
+                        </div>
+                        <div className="footer-info">
+                            <h1>03</h1>
+                            <h2>The Growth of Gaming</h2>
+                            <p>How the pandemic has sparked fresh opportunities.</p>
+                        </div>
                     </div>
                 </div>
             </div>
